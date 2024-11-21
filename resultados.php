635aa15da060b450c4d488cd658fbbbcdc3dc1d7
ferreyralorenzo2 <ferreyralorenzo2@gmail.com>
2024-11-21 00:34:44 +0000

    Commit con correcciones

--- resultados.php
+++ resultados.php
@@ -1,12 +1,26 @@
-<?php
-if (isset ($_POST["Valoración"])) {
-    echo "Valoración". $_POST["Valoración"];
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Resultados</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <?php
+    if (isset($_POST["Valoración"])) {
+        echo "<h2>Valoración: " . $_POST["Valoración"] . "</h2>";
     }
 
-    if (isset ($_POST["autor"])) {
-        echo "autor". $_POST["autor"];
-}
-?>
-
-
-
+    if (isset($_POST["autor"])) {
+        echo "<h2>Autores que conocías:</h2>";
+        echo "<ul>";
+        foreach($_POST["autor"] as $autor) {
+            echo "<li>" . $autor . "</li>";
+        }
+        echo "</ul>";
+    }
+    
+    echo "<p><a href='index.html'>Volver al inicio</a></p>";
+    ?>
+</body>
+</html>
